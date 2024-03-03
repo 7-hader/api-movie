54d7ff3b86959e8179a4aa037a4b2f79529f3a09
Jhon Morales <hader.seven@gmail.com>
2024-03-03 14:54:55 -0500

    consumo avances de películas en tendencia

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-console.log(API_KEY);
+async function getTrendingMoviesPreview() {
+    const resp = await fetch(
+        'https://api.themoviedb.org/3/trending/movie/day?api_key=' 
+        + API_KEY
+        )
+    const data = await resp.json()
+    const movies = data.results
+
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute(
+        'src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+        );
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+}
+
+getTrendingMoviesPreview()
