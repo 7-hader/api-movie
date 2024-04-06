5deda632b5a8875c9134b70f301597835c592ec1
Jhon Morales <hader.seven@gmail.com>
2024-04-06 13:15:10 -0500

    implementacióÃn botón cargar máÃs películas

--- src/main.js
+++ src/main.js
@@ -20,8 +20,18 @@ const lazyLoader = new IntersectionObserver((entries) => {
   })
 })
 
-function createMovies(movies, container, lazyLoad = false) {
-  container.innerHTML = ''
+function createMovies(
+  movies, 
+  container, 
+  {
+    lazyLoad = false,
+    clean = true,
+  } = {},
+) {
+  
+  if (clean) {
+    container.innerHTML = ''
+  }
 
   movies.forEach(movie => {    
     const movieContainer = document.createElement('div');
@@ -116,7 +126,31 @@ async function getTrendingMovies() {
   const { data } = await api('trending/movie/day')
   const movies = data.results
 
-  createMovies(movies, genericSection);
+  createMovies(movies, genericSection, { lazyLoad:true, clean:true });
+
+  const btnLoadMore = document.createElement('button')
+  btnLoadMore.textContent = 'Cargar más'
+  btnLoadMore.addEventListener('click', getPaginatedTrendingMovies)
+  genericSection.appendChild(btnLoadMore)
+}
+
+let page = 1;
+
+async function getPaginatedTrendingMovies() {
+  page++;
+  const { data } = await api('trending/movie/day', {
+    params: {
+      page,
+    },
+  });
+  const movies = data.results
+
+  createMovies(movies, genericSection, { lazyLoad:true, clean:false });
+
+  const btnLoadMore = document.createElement('button')
+  btnLoadMore.textContent = 'Cargar más'
+  btnLoadMore.addEventListener('click', getPaginatedTrendingMovies)
+  genericSection.appendChild(btnLoadMore)
 }
 
 async function getMovieById(id) {
