89ddc189de451093b3fa188962050a09ff3bd913
Jhon Morales <hader.seven@gmail.com>
2024-04-09 01:34:24 -0500

    agregación sección favoritos y botón like

--- src/main.js
+++ src/main.js
@@ -36,10 +36,7 @@ function createMovies(
   movies.forEach(movie => {    
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
-    movieContainer.addEventListener('click', () => {
-      location.hash = '#movie=' + movie.id
-    })
-
+    
     const movieImg = document.createElement('img');
     movieImg.classList.add('movie-img');
     movieImg.setAttribute('alt', movie.title);
@@ -47,18 +44,30 @@ function createMovies(
       lazyLoad ? 'data-img' : 'src',
       'https://image.tmdb.org/t/p/w300' + movie.poster_path,
     );
+    movieImg.addEventListener('click', () => {
+      location.hash = '#movie=' + movie.id
+    });
     movieImg.addEventListener('error', () => {
       movieImg.setAttribute(
         'src', 
         `https://via.placeholder.com/300x450/EEEAF2/3E0F64?text=No disponible`
       )
-    })
+    });
+
+    const movieBtn = document.createElement('button');
+    movieBtn.classList.add('movie-btn');
+    movieBtn.addEventListener('click', () => {
+      movieBtn.classList.toggle('movie-btn--liked');
+      // 
+    });
+
 
     if (lazyLoad) {
       lazyLoader.observe(movieImg)
     }
 
     movieContainer.appendChild(movieImg);
+    movieContainer.appendChild(movieBtn);
     container.appendChild(movieContainer);
   });
 }
